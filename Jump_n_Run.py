b640df26068db5ee8e2cbc30888bda140f42d8a5
Gerd Harlander <gerd.harlander@me.com>
2020-05-31 18:00:17 +0200

    Jump eingefügt

--- Jump_n_Run.py
+++ Jump_n_Run.py
@@ -2,29 +2,50 @@
 import pygame
 import sys
 
+#Init
 pygame.init()
-screen=pygame.display.set_mode([600,600])
+hintergrund=pygame.image.load("Grafiken/hintergrund.png")
+screen=pygame.display.set_mode([1200,595])
+clock=pygame.time.Clock()
+pygame.display.set_caption("Jumpy by Gerd Harlander")
 
+def zeichnen():
+    screen.blit(hintergrund,(0,0))
+    pygame.draw.rect(screen,(0,0,255),(x,y,breite,hoehe))
+    pygame.display.update()
+
+#Variables
 x=300
-y=300
+y=440
+gesch=5
+breite=40
+hoehe=80
+sprungvar=-16
 
-gesch=3
-breite=30
-hoehe=40
+linkeWand=pygame.draw.rect(screen,(0,0,0),(-2,0,2,600),0)
+rechteWand=pygame.draw.rect(screen,(0,0,0),(1201,0,2,600),0)
 
+#Main Game Loop
 go=True
-
 while go:
+    #Event Sink
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit
+    spielerRechteck=pygame.Rect(x,y,40,80)
+    gedrueckt=pygame.key.get_pressed()
+    if gedrueckt[pygame.K_UP] and sprungvar==-16:
+        sprungvar=15
+    if gedrueckt[pygame.K_RIGHT] and not spielerRechteck.colliderect(rechteWand):
+        x+=gesch
+    if gedrueckt[pygame.K_LEFT] and not spielerRechteck.colliderect(linkeWand):
+        x-=gesch
+    if sprungvar>=-15:
+        n=1
+        if sprungvar<0:
+            n=-1
+        y-=(sprungvar**2)*0.17*n
+        sprungvar-=1
 
-        gedruckt=pygame.key.get_pressed()
-        if gedruckt[pygame.K_UP]:
-            y-=gesch
-        if gedruckt[pygame.K_DOWN]:
-            y+=gesch
-        if gedruckt[pygame.K_RIGHT]:
-            x+=gesch
-        if gedruckt[pygame.K_LEFT]:
-            x-=gesch
+    zeichnen()
+    clock.tick(60)
