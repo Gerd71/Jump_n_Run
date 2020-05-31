9380230f4b3ff6577f1cd214f821c6bf1220e7d0
Gerd Harlander <gerd.harlander@me.com>
2020-05-31 18:37:17 +0200

    Comments Added on Animations

--- Jump_n_Run.py
+++ Jump_n_Run.py
@@ -9,9 +9,11 @@ screen=pygame.display.set_mode([1200,595])
 clock=pygame.time.Clock()
 pygame.display.set_caption("Jumpy by Gerd Harlander")
 
+#Load png´s for the 4 different states
 stehen=pygame.image.load("Grafiken/stand.png")
 sprung=pygame.image.load("Grafiken/sprung.png")
 
+#List with 8 pictures for Animation
 rechtsGehen = [pygame.image.load("Grafiken/rechts1.png"),pygame.image.load("Grafiken/rechts2.png"),pygame.image.load("Grafiken/rechts3.png"),pygame.image.load("Grafiken/rechts4.png"),pygame.image.load("Grafiken/rechts5.png"),pygame.image.load("Grafiken/rechts6.png"),pygame.image.load("Grafiken/rechts7.png"),pygame.image.load("Grafiken/rechts8.png")]
 linksGehen = [pygame.image.load("Grafiken/links1.png"),pygame.image.load("Grafiken/links2.png"),pygame.image.load("Grafiken/links3.png"),pygame.image.load("Grafiken/links4.png"),pygame.image.load("Grafiken/links5.png"),pygame.image.load("Grafiken/links6.png"),pygame.image.load("Grafiken/links7.png"),pygame.image.load("Grafiken/links8.png")]
 
@@ -26,15 +28,19 @@ def zeichnen(liste):
     if schritteLinks==63:
         schritteLinks=0
 
+    #Schritte Links animation
     if liste[0]:
         screen.blit(linksGehen[schritteLinks//8],(x,y))
-
+    #Schritte Rechts animation
     if liste[1]:
         screen.blit(rechtsGehen[schritteRechts//8],(x,y))
+    #Stehen animation
     if liste[2]:
         screen.blit(stehen,(x,y))
+    #Springen animation
     if liste[3]:
         screen.blit(sprung,(x,y))
+
     pygame.display.update()
 
 #Variables
@@ -45,10 +51,11 @@ breite=40
 hoehe=80
 sprungvar=-16
 
+#Field Boarders
 linkeWand=pygame.draw.rect(screen,(0,0,0),(-2,0,2,600),0)
 rechteWand=pygame.draw.rect(screen,(0,0,0),(1201,0,2,600),0)
 
-#[links,rechts,stand,sprung]
+#States for player [links,rechts,stand,sprung]
 richtg=[0,0,0,0]
 schritteRechts=0
 schritteLinks=0
@@ -62,27 +69,34 @@ while go:
             sys.exit
     spielerRechteck=pygame.Rect(x,y,40,80)
     gedrueckt=pygame.key.get_pressed()
+    #status Stehen
     richtg=[0,0,1,0]
     if gedrueckt[pygame.K_UP] and sprungvar==-16:
         sprungvar=15
     if gedrueckt[pygame.K_RIGHT] and not spielerRechteck.colliderect(rechteWand):
         x+=gesch
+        #status Rechts
         richtg=[0,1,0,0]
         schritteRechts+=1
     if gedrueckt[pygame.K_LEFT] and not spielerRechteck.colliderect(linkeWand):
         x-=gesch
+        #status Links
         richtg=[1,0,0,0]
         schritteLinks+=1
     if sprungvar>=-15:
+        #status Sprung
         richtg=[0,0,0,1]
         n=1
         if sprungvar<0:
             n=-1
         y-=(sprungvar**2)*0.17*n
         sprungvar-=1
+    
+    #reset if Stehen or Sprung
     if richtg[2] or richtg[3]:
         schritteRechts=0
         schritteLinks=0
         
     zeichnen(richtg)
+
     clock.tick(60)
